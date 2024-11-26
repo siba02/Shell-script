6a6804c182c8a346a8199bdc2f96057b037311cd
siba02 <sibaprasad.tofan@gmail.com>
2024-11-26 19:24:56 +0530

    some new lines added

---
+++ comment.sh
@@ -0,0 +1,17 @@
+#!/bin/bash                               
+welcome:home                            #the second line is printed but the third line is not going to be printed because of the comment(#).
+#welcome:to the party.ls                   
+echo "welcome to # scripting"             # if we write # inside of a printing line (inside quote)it is going to be printed as it is.
+
+echo hii this is siba #from Talcher       #but here the echo doesn't print the from talcher ,as a part of comment.
+
+echo "today
+i
+am
+going
+to
+the
+party"                              #printing multiline string  
+
+
+
