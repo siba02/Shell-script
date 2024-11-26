8592c480a1520785ca9429f8a0c4fcae35bfac50
siba02 <sibaprasad.tofan@gmail.com>
2024-11-26 18:51:36 +0530

    add new file

--- first.sh
+++ first.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-echo "Hello this is siba"
+echo "Hello this is siba"   #echo is use to display the text or variable on the terminal.
+
+echo -e "\033[0;31m printed fail message " 
+echo -e "\033[0;32m printed success message " #echo -e is enable interpretation of backslash-escaped characters.
+echo -e "\033[0;33m printed warning message "
