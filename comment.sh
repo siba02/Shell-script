3d20078a98486134f1fa79a5a3059d3fdde5436d
siba02 <sibaprasad.tofan@gmail.com>
2024-11-27 00:06:31 +0530

    some changes

--- comment.sh
+++ comment.sh
@@ -5,17 +5,29 @@ echo "welcome to # scripting"             # if we write # inside of a printing l
 
 echo hii this is siba #from Talcher       #but here the echo doesn't print the from talcher ,as a part of comment.
 
+
+# printing multiline string
 echo "today
 i
 am
-going
+going                                       
 to
 the
-party"                              #printing multiline string  
+party"   
 
+# backslash.
 echo "Recently \
 i \
 completed \
 my \
 graduation "                          #here the the output is going to be printed in a single line because of the backslash(\).
 
+#strong quote.
+echo 'Recently \
+i \
+completed \
+my \
+graduation'                          #here the the output is going to be printed as it is because of single quote(').
+
+
+echo -e "Now we have \vsome interesting things\v to work on "       #vertical tab  
