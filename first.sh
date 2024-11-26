4855465a44921dd61e917382ddce87d353fbd673
siba02 <sibaprasad.tofan@gmail.com>
2024-11-27 01:23:58 +0530

    some changes

--- first.sh
+++ first.sh
@@ -4,3 +4,7 @@ echo "Hello this is siba"   #echo is use to display the text or variable on the 
 echo -e "\033[0;31m printed fail message " 
 echo -e "\033[0;32m printed success message " #echo -e is enable interpretation of backslash-escaped characters.
 echo -e "\033[0;33m printed warning message "
+
+
+echo $SHELL   #for checking current shell
+echo $PATH     #for checking path
