26a99c771fd135140308ce8fa6dab256011721d3
siba02 <sibaprasad.tofan@gmail.com>
2024-12-03 16:04:20 +0530

    new file added

---
+++ assign-command-output.sh
@@ -0,0 +1,13 @@
+# pwd
+
+working_directory="pwd"          #here we tried to store the value of pwd in working_directory and print the working directory ,then it print "pwd" insted of its value.
+
+echo ${working_directory}
+
+
+directory=$(pwd)  #`pwd`          #here it store the value of pwd in directory and print successfully.
+
+echo ${directory}
+
+date_time=$(date +"%D-%T")
+echo ${date_time}
